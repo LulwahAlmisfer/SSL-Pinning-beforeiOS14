f90b5348361c90fde977a2e058e9dd0b06e00ac1
lulu <lulukalmisfer@hotmail.com>
2024-02-18 08:12:48 +0300

    Initial Commit

---
+++ SSLpinning_before_iOS14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSLpinning_before_iOS14
+//
+//  Created by Lulwah Almisfer on 18/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
